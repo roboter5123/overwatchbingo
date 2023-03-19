2281f2cb7996b491eee165df944c3d49583c6c5e
roboter5123 <pascal.roboter@hotmail.com>
2023-03-19 14:50:34 +0100

    added an error message on desktop

--- src/DesktopView.jsx
+++ src/DesktopView.jsx
@@ -1,5 +1,5 @@
 export function MobileView() {
     return (
-        <></>
+        <>{"desktopView has not been implemented yet. Pelse switch to mobile or resize your window to a max width of 500 px"}</>
     );
 }
