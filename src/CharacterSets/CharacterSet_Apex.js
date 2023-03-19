1f3645e4605493b7f56e7501c9937e835e0774c5
roboter5123 <pascal.roboter@hotmail.com>
2023-03-19 22:22:27 +0100

    finished addin apex characters

--- src/CharacterSets/CharacterSet_Apex.js
+++ src/CharacterSets/CharacterSet_Apex.js
@@ -1,11 +1,36 @@
 export const characterSet_Apex = {
     "assault": {
         "ash": false,
-        "bangalore":false,
-        "fuse":false,
-        "madmaggie":false,
-        "revenant":false
+        "bangalore": false,
+        "fuse": false,
+        "madmaggie": false,
+        "revenant": false
     },
-    "recon":{"bloodhound":false,"crypto":false,"seer":false,"vantage":false}
+    "skirmisher": {
+        "horizon":false,
+        "mirage":false,
+        "octane":false,
+        "pathfinder":false,
+        "valkyrie":false,
+        "wraith":false
+    },
+    "recon": {
+        "bloodhound": false,
+        "crypto": false,
+        "seer": false,
+        "vantage": false
+    },
+    "controller":{
+        "catalyst":false,
+        "caustic":false,
+        "rampart":false,
+        "wattson":false,
+    },
+    "support":{
+        "gibraltar":false,
+        "lifeline":false,
+        "loba":false,
+        "newcastle":false
+    }
 
 }
